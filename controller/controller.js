4a9acac0b5e0cda0104b8504d5e8dba912513434
Gabriel Almeida <gabrielrnalmd@gmail.com>
2025-09-30 00:25:52 -0300

    alterações "id"

--- controller/controller.js
+++ controller/controller.js
@@ -2,63 +2,72 @@ $(document).ready(function () {
     const container = $("#main_content");
     const products = [
         {
+            id: "prod-1",
             img: "images/products/product-2.png",
             title: "Dell Inspiron 15",
             price: "R$ 4.500",
             desc: "Dell Inspiron 15 (3000 Series): notebook básico com tela de 15,6\", webcam integrada e design clássico em preto."
         },
         {
+            id: "prod-2",
             img: "images/products/product-8.png",
             title: "iPhone 14 Pro",
             price: "R$ 6.299",
             desc: "iPhone 14 Pro (ou Pro Max): smartphone “Pro” da Apple lançado em 2022, com A16 Bionic, tela ProMotion."
         },
         {
+            id: "prod-3",
             img: "images/products/product-14.png",
             title: "PlayStation 5",
             price: "R$ 4.000",
             desc: "PlayStation 5 (com drive de disco): console da Sony lançado em 2020, com CPU/GPU customizados."
         },
         {
+            id: "prod-4",
             img: "images/products/product-9.png",
             title: "Galaxy S23 Ultra",
             price: "R$ 4.179",
             desc: "Samsung Galaxy S23 Ultra: flagship de 2023, com tela Dynamic AMOLED 2X de 6,8″, taxa adaptativa de 120 Hz."
         },
         {
+            id: "prod-5",
             img: "images/products/product-15.png",
             title: "Aorus Supreme 5",
             price: "R$ 4.182",
             desc: "Chassi com painéis de vidro temperado, iluminação RGB sincronizável via RGB Fusion, conectividade de ponta."
         },
         {
+            id: "prod-6",
             img: "images/products/product-7.png",
             title: "Pop Racer X",
             price: "R$ 688,90",
             desc: "Cadeira gamer estilo racing. Design inspirado em bancos esportivos com encosto alto, apoio para cabeça."
         },
         {
+            id: "prod-7",
             img: "images/products/product-13.png",
             title: "Thrustmaster T300",
             price: "R$ 4.214,75",
             desc: "Volante estilo Ferrari com pedais: periférico de simulação licenciado pela Ferrari, com volante de direção."
         },
         {
+            id: "prod-8",
             img: "images/products/product-10.png",
             title: "ROG Chill Case",
             price: "R$ 147",
             desc: "AeroActive Cooler (clip-on): ventilador externo com sistema termoelétrico nos modelos mais avançados."
         },
         {
+            id: "prod-9",
             img: "images/products/product-1.png",
             title: "Asus VivoBook 15",
             price: "R$ 2.999",
             desc: "Asus VivoBook 15 (Slate Gray): notebook fino e leve com tela de ~15,6”, teclado completo incluindo teclado."
         }
     ];
-    products.forEach((prod, index) => {
+    products.forEach(prod => {
         const cardHTML = `
-            <div class="card card${index + 1}">
+            <div id="${prod.id}"  class="card card">
                 <img src="${prod.img}" alt="${prod.title}">
                 <h1>${prod.title} <br> ${prod.price}</h1>
                 <p>${prod.desc}</p>
